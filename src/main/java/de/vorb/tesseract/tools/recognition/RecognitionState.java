c9b6fe43dc45d2d66cc707350aee65ec4d45589a
Paul Vorbach <paul@vorba.ch>
2014-04-13 22:19:44 +0200

    RecognitionState.getBaseline() and .getConfidence()

--- src/main/java/de/vorb/tesseract/tools/recognition/RecognitionState.java
+++ src/main/java/de/vorb/tesseract/tools/recognition/RecognitionState.java
@@ -4,6 +4,7 @@ import org.bridj.Pointer;
 
 import de.vorb.tesseract.bridj.Tesseract;
 import de.vorb.tesseract.bridj.Tesseract.TessPageIteratorLevel;
+import de.vorb.tesseract.tools.util.Baseline;
 import de.vorb.tesseract.tools.util.Box;
 
 public class RecognitionState {
@@ -57,5 +58,42 @@ public class RecognitionState {
         level).getCString();
   }
 
-  // TODO implement more getters
+  /**
+   * Get the baseline information of the given iterator level.
+   * 
+   * @param level
+   *          level of the requested baseline
+   * @return baseline
+   */
+  public Baseline getBaseline(TessPageIteratorLevel level) {
+    // pointers to the baseline coordinates
+    final Pointer<Integer> x1 = Pointer.allocateInt();
+    final Pointer<Integer> y1 = Pointer.allocateInt();
+    final Pointer<Integer> x2 = Pointer.allocateInt();
+    final Pointer<Integer> y2 = Pointer.allocateInt();
+
+    Tesseract.TessPageIteratorBaseline(pageIt, level, x1, y1, x2, y2);
+
+    final int width = x2.getInt() - x1.getInt();
+    final float height = y2.getInt() - y1.getInt();
+    final float slope = height / width;
+
+    final Box bbox = getBoundingBox(TessPageIteratorLevel.RIL_WORD);
+    final int yOffset = bbox.getY() + bbox.getHeight() - y1.getInt();
+
+    return new Baseline(yOffset, slope);
+  }
+
+  /**
+   * Get the confidence of the given iterator level.
+   * 
+   * @param level
+   *          level of the requested confidence
+   * @return recognition confidence
+   */
+  public float getConfidence(TessPageIteratorLevel level) {
+    return Tesseract.TessResultIteratorConfidence(resultIt, level);
+  }
+
+  // TODO more getters
 }
