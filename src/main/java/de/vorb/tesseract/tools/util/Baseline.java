3806ce0472fbf6f038070f2b8a80cf89d3b0190f
Paul Vorbach <paul@vorba.ch>
2014-04-13 22:20:26 +0200

    Crossline -> Straight

--- src/main/java/de/vorb/tesseract/tools/util/Baseline.java
+++ src/main/java/de/vorb/tesseract/tools/util/Baseline.java
@@ -1,6 +1,6 @@
 package de.vorb.tesseract.tools.util;
 
-public class Baseline extends Crossline {
+public class Baseline extends Straight {
   public Baseline(int yOffset, float slope) {
     super(yOffset, slope);
   }
