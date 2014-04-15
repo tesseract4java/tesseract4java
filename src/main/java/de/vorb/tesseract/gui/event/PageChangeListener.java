90e12740646d3a5c6568422402ac1cfec7636339
Paul Vorbach <paul@vorba.ch>
2014-04-15 10:11:29 +0200

    Use Path instead of String

--- src/main/java/de/vorb/tesseract/gui/event/PageChangeListener.java
+++ src/main/java/de/vorb/tesseract/gui/event/PageChangeListener.java
@@ -1,5 +1,7 @@
 package de.vorb.tesseract.gui.event;
 
+import java.nio.file.Path;
+
 public interface PageChangeListener {
-  public void pageChanged(int pageIndex, String page);
+  public void pageChanged(int pageIndex, Path page);
 }
