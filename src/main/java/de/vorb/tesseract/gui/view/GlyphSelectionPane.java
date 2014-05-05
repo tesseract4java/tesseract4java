e7e4d85ab58bc32fd7e811b9efb6802948290ab9
Paul Vorbach <paul@vorba.ch>
2014-05-06 01:07:35 +0200

    Use Set instead of List

--- src/main/java/de/vorb/tesseract/gui/view/GlyphSelectionPane.java
+++ src/main/java/de/vorb/tesseract/gui/view/GlyphSelectionPane.java
@@ -1,24 +1,23 @@
 package de.vorb.tesseract.gui.view;
 
-import javax.swing.JPanel;
-
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.util.Map.Entry;
+import java.util.Set;
 
-import javax.swing.JScrollPane;
-import javax.swing.JList;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 
 import de.vorb.tesseract.gui.view.renderer.GlyphSelectionRenderer;
 import de.vorb.tesseract.util.Symbol;
 
-import java.awt.FlowLayout;
-import java.util.List;
-import java.util.Map.Entry;
-
 public class GlyphSelectionPane extends JPanel {
     private static final long serialVersionUID = 1L;
 
-    private final JList<Entry<String, List<Symbol>>> selectionList;
+    private final JList<Entry<String, Set<Symbol>>> selectionList;
 
     /**
      * Create the panel.
@@ -39,11 +38,13 @@ public class GlyphSelectionPane extends JPanel {
         add(scrollPane, BorderLayout.CENTER);
 
         selectionList = new JList<>();
+        selectionList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
         selectionList.setCellRenderer(new GlyphSelectionRenderer());
         scrollPane.setViewportView(selectionList);
     }
 
-    public JList<Entry<String, List<Symbol>>> getList() {
+    public JList<Entry<String, Set<Symbol>>> getList() {
         return selectionList;
     }
 }
