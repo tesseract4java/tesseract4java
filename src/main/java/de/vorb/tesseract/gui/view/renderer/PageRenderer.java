c77887314c0d3772b1f9fb3237d8e745f87ac069
Paul Vorbach <paul@vorba.ch>
2014-05-15 17:02:37 +0200

    Improve PageRenderer

--- src/main/java/de/vorb/tesseract/gui/view/renderer/PageRenderer.java
+++ src/main/java/de/vorb/tesseract/gui/view/renderer/PageRenderer.java
@@ -1,7 +1,23 @@
 package de.vorb.tesseract.gui.view.renderer;
 
+import java.awt.image.BufferedImage;
+
 import de.vorb.tesseract.util.Page;
 
+/**
+ * Page renderer.
+ * 
+ * @author Paul Vorbach
+ */
 public interface PageRenderer {
-    public void render(Page page);
+
+    /**
+     * Renders the information of a page on an optionally given background.
+     * 
+     * @param page
+     *            page model to render
+     * @param pageBackground
+     *            background image to render below (may also be null)
+     */
+    public void render(Page page, BufferedImage pageBackground);
 }
