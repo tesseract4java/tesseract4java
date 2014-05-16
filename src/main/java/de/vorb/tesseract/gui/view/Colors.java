7a43e5c09c5caf3a844643b2a55f95682ddaf519
Paul Vorbach <paul@vorba.ch>
2014-05-16 11:21:34 +0200

    Export static members into own classes

---
+++ src/main/java/de/vorb/tesseract/gui/view/Colors.java
@@ -0,0 +1,14 @@
+package de.vorb.tesseract.gui.view;
+
+import java.awt.Color;
+
+public class Colors {
+    private Colors() {
+    }
+
+    public static final Color CORRECT = new Color(0xFF66CC00);
+    public static final Color INCORRECT = Color.RED;
+    public static final Color BASELINE = Color.BLUE;
+    public static final Color TEXT = Color.BLACK;
+    public static final Color LINE_NUMBER = Color.GRAY;
+}
---
+++ src/main/java/de/vorb/tesseract/gui/view/Strokes.java
@@ -0,0 +1,12 @@
+package de.vorb.tesseract.gui.view;
+
+import java.awt.BasicStroke;
+import java.awt.Stroke;
+
+public class Strokes {
+    private Strokes() {
+    }
+
+    public static final Stroke NORMAL = new BasicStroke(1);
+    public static final Stroke SELECTION = new BasicStroke(3);
+}
