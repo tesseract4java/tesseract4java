14d819bd40f8d3aaaa0c315abc8085c6ed07d1be
Paul Vorbach <paul@vorba.ch>
2014-05-19 10:34:46 +0200

    Organize imports

--- src/main/java/de/vorb/tesseract/gui/view/TesseractFrame.java
+++ src/main/java/de/vorb/tesseract/gui/view/TesseractFrame.java
@@ -13,7 +13,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
-import java.util.Comparator;
 import java.util.Locale;
 import java.util.Map.Entry;
 import java.util.ResourceBundle;
