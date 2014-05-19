433500164cfa31b720404d11491656c2ca0d166d
Paul Vorbach <paul@vorba.ch>
2014-05-19 10:43:53 +0200

    Remove unused var

--- src/main/java/de/vorb/tesseract/util/Project.java
+++ src/main/java/de/vorb/tesseract/util/Project.java
@@ -8,67 +8,66 @@ import java.util.List;
 import de.vorb.tesseract.gui.event.PageChangeListener;
 
 public class Project {
-  private final Path scanDir;
-  private final List<Path> pages;
-  private int pageIndex = 0;
-  private final List<PageChangeListener> pageChangeListeners;
+    private final Path scanDir;
+    private final List<Path> pages;
+    private int pageIndex = 0;
+    private final List<PageChangeListener> pageChangeListeners;
 
-  public Project(Path scanDir, List<Path> pages) {
-    this.scanDir = scanDir;
-    this.pages = pages;
+    public Project(Path scanDir, List<Path> pages) {
+        this.scanDir = scanDir;
+        this.pages = pages;
 
-    this.pageChangeListeners = new LinkedList<PageChangeListener>();
-  }
-
-  public Path getScanDir() {
-    return scanDir;
-  }
-
-  public List<Path> getPages() {
-    return Collections.unmodifiableList(this.pages);
-  }
-
-  public void addPageChangeListener(PageChangeListener listener) {
-    pageChangeListeners.add(listener);
-  }
-
-  public void removePageChangeListener(PageChangeListener listener) {
-    pageChangeListeners.remove(listener);
-  }
-
-  public int getSelectedPageIndex() {
-    return pageIndex;
-  }
-
-  public int getMinimumPageIndex() {
-    return 0;
-  }
-
-  public int getMaximumPageIndex() {
-    return Math.max(pages.size() - 1, 0);
-  }
-
-  public Path getSelectedPage() {
-    return pages.get(getSelectedPageIndex());
-  }
-
-  private void pageChanged() {
-    final int index = getSelectedPageIndex();
-    final Path page = getSelectedPage();
-
-    for (PageChangeListener listener : pageChangeListeners) {
-      listener.pageSelectionChanged(index);
+        this.pageChangeListeners = new LinkedList<PageChangeListener>();
     }
-  }
 
-  public void setSelectedPageIndex(int index) {
-    if (index < 0)
-      throw new IllegalArgumentException("index < 0");
-    if (index > getMaximumPageIndex())
-      throw new IllegalArgumentException("index > maxIndex");
+    public Path getScanDir() {
+        return scanDir;
+    }
 
-    pageIndex = index;
+    public List<Path> getPages() {
+        return Collections.unmodifiableList(this.pages);
+    }
 
-    pageChanged();
-  }
+    public void addPageChangeListener(PageChangeListener listener) {
+        pageChangeListeners.add(listener);
+    }
+
+    public void removePageChangeListener(PageChangeListener listener) {
+        pageChangeListeners.remove(listener);
+    }
+
+    public int getSelectedPageIndex() {
+        return pageIndex;
+    }
+
+    public int getMinimumPageIndex() {
+        return 0;
+    }
+
+    public int getMaximumPageIndex() {
+        return Math.max(pages.size() - 1, 0);
+    }
+
+    public Path getSelectedPage() {
+        return pages.get(getSelectedPageIndex());
+    }
+
+    private void pageChanged() {
+        final int index = getSelectedPageIndex();
+
+        for (PageChangeListener listener : pageChangeListeners) {
+            listener.pageSelectionChanged(index);
+        }
+    }
+
+    public void setSelectedPageIndex(int index) {
+        if (index < 0)
+            throw new IllegalArgumentException("index < 0");
+        if (index > getMaximumPageIndex())
+            throw new IllegalArgumentException("index > maxIndex");
+
+        pageIndex = index;
+
+        pageChanged();
+    }
 }
