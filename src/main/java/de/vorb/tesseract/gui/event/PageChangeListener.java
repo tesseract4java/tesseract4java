4a4375c93754f88a23c82c15120fc0c6fd306cdb
Paul Vorbach <paul@vorba.ch>
2014-05-19 10:32:53 +0200

    Simplify PageChangeListener

--- src/main/java/de/vorb/tesseract/gui/event/PageChangeListener.java
+++ src/main/java/de/vorb/tesseract/gui/event/PageChangeListener.java
@@ -1,7 +1,5 @@
 package de.vorb.tesseract.gui.event;
 
-import java.nio.file.Path;
-
 public interface PageChangeListener {
-  public void pageChanged(int pageIndex, Path page);
+  public void pageSelectionChanged(int pageIndex);
 }
