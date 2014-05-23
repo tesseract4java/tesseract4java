5503a6de7afbba092fef9bcaac038b4c516bc615
Paul Vorbach <paul@vorba.ch>
2014-05-23 10:58:46 +0200

    languageChanged() can throw IOExceptions

--- src/main/java/de/vorb/tesseract/gui/event/LanguageChangeListener.java
+++ src/main/java/de/vorb/tesseract/gui/event/LanguageChangeListener.java
@@ -1,5 +1,7 @@
 package de.vorb.tesseract.gui.event;
 
+import java.io.IOException;
+
 public interface LanguageChangeListener {
-    public void languageChanged(String language);
+    public void languageChanged(String language) throws IOException;
 }
