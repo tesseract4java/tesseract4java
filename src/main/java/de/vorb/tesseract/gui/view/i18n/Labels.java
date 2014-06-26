b8b44a827dec3b99a719a83e60e07ab174da9e64
Paul Vorbach <paul@vorba.ch>
2014-06-26 22:28:35 +0200

    Replace with previous version
    Providing default labels will lead to outdated label definition files

--- src/main/java/de/vorb/tesseract/gui/view/i18n/Labels.java
+++ src/main/java/de/vorb/tesseract/gui/view/i18n/Labels.java
@@ -6,17 +6,13 @@ import java.util.ResourceBundle;
 
 public class Labels {
     public static String getLabel(Locale locale, String key) {
-        return getLabel(locale, key, "?");
-    }
-
-    public static String getLabel(Locale locale, String key, String defaultValue) {
         final ResourceBundle labels = ResourceBundle.getBundle("l10n/labels",
                 locale);
 
         try {
             return labels.getString(key);
         } catch (MissingResourceException e) {
-            return defaultValue;
+            return "?";
         }
     }
 }
