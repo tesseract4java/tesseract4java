7f9c288ef52a1bcd5c309128484479a3589af69c
Paul Vorbach <paul@vorba.ch>
2014-06-27 15:14:05 +0200

    Redesign NewProjectDialog

--- src/main/java/de/vorb/tesseract/gui/view/NewProjectDialog.java
+++ src/main/java/de/vorb/tesseract/gui/view/NewProjectDialog.java
@@ -1,184 +1,160 @@
 package de.vorb.tesseract.gui.view;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.Window;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
 
-import de.vorb.tesseract.gui.event.LocaleChangeListener;
 import de.vorb.tesseract.gui.event.ProjectChangeListener;
-import de.vorb.tesseract.gui.util.Resources;
-import de.vorb.tesseract.gui.view.i18n.Labels;
-
-import java.awt.Toolkit;
+import javax.swing.border.TitledBorder;
+import javax.swing.JCheckBox;
 
 public class NewProjectDialog extends JDialog {
+    public static enum ResultState {
+        APPROVE, CANCEL, ERROR;
+    }
+
     private static final long serialVersionUID = 1L;
 
-    private final JPanel contentPanel = new JPanel();
-    private final JTextField tfScanDir;
+    protected ResultState resultState;
 
     private final List<ProjectChangeListener> listeners = new LinkedList<ProjectChangeListener>();
-
-    private final JButton btCancel;
-    private final JButton btOK;
-    private final JLabel lblScanDirectory;
+    private JTextField tfPath;
 
     /**
      * Create the dialog.
      * 
      * @param owner
      */
-    public NewProjectDialog(final Window owner) {
+    private NewProjectDialog(final Window owner) {
         super(owner);
-        setTitle(Labels.getLabel(getLocale(), "new_project_dialog_title"));
+        setResizable(false);
         setIconImage(Toolkit.getDefaultToolkit().getImage(
                 NewProjectDialog.class.getResource("/logos/logo_16.png")));
+        setTitle("New Project");
 
-        setMinimumSize(new Dimension(500, 130));
+        JPanel panel = new JPanel();
+        getContentPane().add(panel);
+        panel.setLayout(new BorderLayout(0, 0));
 
-        getContentPane().setLayout(new BorderLayout());
-        contentPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-        getContentPane().add(contentPanel, BorderLayout.CENTER);
-        GridBagLayout gbl_contentPanel = new GridBagLayout();
-        gbl_contentPanel.columnWidths = new int[] { 0, 0, 0, 0 };
-        gbl_contentPanel.rowHeights = new int[] { 0, 0 };
-        gbl_contentPanel.columnWeights = new double[] { 0.0, 1.0, 0.0,
+        JPanel top = new JPanel();
+        top.setBorder(new EmptyBorder(10, 10, 10, 10));
+        top.setBackground(SystemColor.window);
+        panel.add(top, BorderLayout.NORTH);
+        FlowLayout fl_top = new FlowLayout(FlowLayout.LEADING, 5, 5);
+        top.setLayout(fl_top);
+
+        JLabel lblCreateNewProject = new JLabel("Create a new Project");
+        lblCreateNewProject.setFont(new Font("Tahoma", Font.BOLD, 13));
+        top.add(lblCreateNewProject);
+
+        JPanel bottom = new JPanel();
+        bottom.setBorder(new EmptyBorder(10, 10, 10, 10));
+        FlowLayout fl_bottom = (FlowLayout) bottom.getLayout();
+        fl_bottom.setAlignment(FlowLayout.TRAILING);
+        panel.add(bottom, BorderLayout.SOUTH);
+
+        JButton btnCreate = new JButton("Create");
+        btnCreate.setEnabled(false);
+        bottom.add(btnCreate);
+
+        JButton btnCancel = new JButton("Cancel");
+        bottom.add(btnCancel);
+
+        JPanel main = new JPanel();
+        main.setBorder(new CompoundBorder(new MatteBorder(1, 0, 1, 0,
+                (Color) new Color(180, 180, 180)), new EmptyBorder(16, 16, 16,
+                16)));
+        panel.add(main, BorderLayout.CENTER);
+        main.setLayout(new BorderLayout(0, 0));
+
+        JPanel directory = new JPanel();
+        directory.setBorder(new EmptyBorder(0, 0, 16, 0));
+        main.add(directory, BorderLayout.NORTH);
+        GridBagLayout gbl_directory = new GridBagLayout();
+        gbl_directory.columnWidths = new int[] { 0, 0, 0, 0 };
+        gbl_directory.rowHeights = new int[] { 23, 0 };
+        gbl_directory.columnWeights = new double[] { 0.0, 1.0, 0.0,
                 Double.MIN_VALUE };
-        gbl_contentPanel.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
-        contentPanel.setLayout(gbl_contentPanel);
-        {
-            lblScanDirectory = new JLabel(Labels.getLabel(getLocale(),
-                    "scan_dir"));
-            GridBagConstraints gbc_lblScans = new GridBagConstraints();
-            gbc_lblScans.insets = new Insets(0, 0, 0, 5);
-            gbc_lblScans.anchor = GridBagConstraints.EAST;
-            gbc_lblScans.gridx = 0;
-            gbc_lblScans.gridy = 0;
-            contentPanel.add(lblScanDirectory, gbc_lblScans);
-        }
-        {
-            tfScanDir = new JTextField();
+        gbl_directory.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
+        directory.setLayout(gbl_directory);
 
-            // TODO remove
-            tfScanDir.setText("E:\\Masterarbeit\\Ressourcen\\DE-20__32_AM_49000_L869_G927-1\\sauvola");
+        JLabel lblDDirectory = new JLabel("Directory:");
+        GridBagConstraints gbc_lblDDirectory = new GridBagConstraints();
+        gbc_lblDDirectory.anchor = GridBagConstraints.WEST;
+        gbc_lblDDirectory.insets = new Insets(0, 0, 0, 5);
+        gbc_lblDDirectory.gridx = 0;
+        gbc_lblDDirectory.gridy = 0;
+        directory.add(lblDDirectory, gbc_lblDDirectory);
 
-            GridBagConstraints gbc_tfScanDir = new GridBagConstraints();
-            gbc_tfScanDir.insets = new Insets(0, 0, 0, 5);
-            gbc_tfScanDir.fill = GridBagConstraints.HORIZONTAL;
-            gbc_tfScanDir.gridx = 1;
-            gbc_tfScanDir.gridy = 0;
-            contentPanel.add(tfScanDir, gbc_tfScanDir);
-            tfScanDir.setColumns(10);
-        }
-        {
-            JButton button = new JButton("...");
-            GridBagConstraints gbc_button = new GridBagConstraints();
-            gbc_button.gridx = 2;
-            gbc_button.gridy = 0;
-            contentPanel.add(button, gbc_button);
+        tfPath = new JTextField();
+        GridBagConstraints gbc_tfPath = new GridBagConstraints();
+        gbc_tfPath.fill = GridBagConstraints.HORIZONTAL;
+        gbc_tfPath.insets = new Insets(0, 0, 0, 5);
+        gbc_tfPath.gridx = 1;
+        gbc_tfPath.gridy = 0;
+        directory.add(tfPath, gbc_tfPath);
+        tfPath.setColumns(10);
 
-            makePathChooser(tfScanDir, button);
-        }
-        {
-            JPanel buttonPane = new JPanel();
-            buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-            getContentPane().add(buttonPane, BorderLayout.SOUTH);
-            {
-                btOK = new JButton();
-                btOK.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        projectChanged();
-                        NewProjectDialog.this.dispose();
-                    }
-                });
-                btOK.setActionCommand("OK");
-                buttonPane.add(btOK);
-                getRootPane().setDefaultButton(btOK);
-            }
-            {
-                btCancel = new JButton();
-                btCancel.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        NewProjectDialog.this.dispose();
-                    }
-                });
-                btCancel.setActionCommand("Cancel");
-                buttonPane.add(btCancel);
-            }
-        }
-        btCancel.setText(Labels.getLabel(getLocale(), "btn_cancel"));
-        btOK.setText(Labels.getLabel(getLocale(), "btn_ok"));
+        JButton tfPathSelect = new JButton("...");
+        GridBagConstraints gbc_tfPathSelect = new GridBagConstraints();
+        gbc_tfPathSelect.anchor = GridBagConstraints.NORTHWEST;
+        gbc_tfPathSelect.gridx = 2;
+        gbc_tfPathSelect.gridy = 0;
+        directory.add(tfPathSelect, gbc_tfPathSelect);
+        tfPathSelect.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                final JFileChooser jfc = new JFileChooser();
+                int result = jfc.showOpenDialog(NewProjectDialog.this);
 
-        this.setResizable(false);
-    }
-
-    private void makePathChooser(final JTextField tfPath,
-            final JButton btnChoosePath) {
-
-        File dir = new File("E:\\Masterarbeit\\Ressourcen");
-        if (!dir.isDirectory())
-            dir = null;
-        final File startDir = dir;
-
-        btnChoosePath.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                final JFileChooser dirChooser = new JFileChooser(startDir);
-                dirChooser.setMultiSelectionEnabled(false);
-                dirChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-
-                int state = dirChooser.showOpenDialog(NewProjectDialog.this);
-                if (state == JFileChooser.APPROVE_OPTION) {
-                    final File selection = dirChooser.getSelectedFile();
-                    tfPath.setText(selection.getAbsolutePath());
-                } else if (state == JFileChooser.ERROR_OPTION) {
-                    JOptionPane.showMessageDialog(dirChooser,
-                            "Please select a directory", "Invalid selection",
-                            JOptionPane.ERROR_MESSAGE);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    tfPath.setText(jfc.getSelectedFile().getAbsolutePath());
                 }
             }
         });
+
+        JPanel options = new JPanel();
+        options.setBorder(new TitledBorder(null, "Options",
+                TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        main.add(options);
+        options.setLayout(new FlowLayout(FlowLayout.LEADING, 5, 5));
+
+        JLabel lblFileFilter = new JLabel("File types:");
+        options.add(lblFileFilter);
+
+        JCheckBox chckbxTiff = new JCheckBox("TIFF");
+        options.add(chckbxTiff);
+
+        JCheckBox chckbxPng = new JCheckBox("PNG");
+        options.add(chckbxPng);
+
+        JCheckBox chckbxJpeg = new JCheckBox("JPEG");
+        options.add(chckbxJpeg);
     }
 
-    public void addProjectChangeListener(ProjectChangeListener listener) {
-        listeners.add(listener);
-    }
+    public static ResultState showDialog(Window parent) {
+        final NewProjectDialog dialog = new NewProjectDialog(parent);
 
-    public void removeProjectChangeListener(ProjectChangeListener listener) {
-        listeners.remove(listener);
-    }
+        dialog.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                dialog.resultState = ResultState.CANCEL;
+            }
+        });
 
-    private void projectChanged() {
-        final Path scanDir = Paths.get(tfScanDir.getText());
-
-        for (ProjectChangeListener l : listeners) {
-            l.projectChanged(scanDir);
-        }
-    }
-
-    @Override
-    public void setVisible(boolean visible) {
-        setLocationRelativeTo(getParent());
-        super.setVisible(visible);
+        return dialog.resultState;
     }
 }
