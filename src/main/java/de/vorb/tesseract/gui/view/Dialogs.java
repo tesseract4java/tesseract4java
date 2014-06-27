cf789bf0bfc248d9d5c9822ab53dee6eb7df0331
Paul Vorbach <paul@vorba.ch>
2014-06-27 16:32:15 +0200

    Correct argument order

--- src/main/java/de/vorb/tesseract/gui/view/Dialogs.java
+++ src/main/java/de/vorb/tesseract/gui/view/Dialogs.java
@@ -7,19 +7,19 @@ import javax.swing.JOptionPane;
 public class Dialogs {
     public static void showInfo(Component parent, String title,
             String message) {
-        JOptionPane.showMessageDialog(parent, title, message,
+        JOptionPane.showMessageDialog(parent, message, title,
                 JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static void showWarning(Component parent, String title,
             String message) {
-        JOptionPane.showMessageDialog(parent, title, message,
+        JOptionPane.showMessageDialog(parent, message, title,
                 JOptionPane.WARNING_MESSAGE);
     }
 
     public static void showError(Component parent, String title,
             String message) {
-        JOptionPane.showMessageDialog(parent, title, message,
+        JOptionPane.showMessageDialog(parent, message, title,
                 JOptionPane.ERROR_MESSAGE);
     }
 }
