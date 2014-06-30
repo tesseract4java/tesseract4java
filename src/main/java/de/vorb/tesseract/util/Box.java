6e6fc6786fb562b1b676f3aba4db3d6645ab0593
Paul Vorbach <paul@vorba.ch>
2014-06-30 10:46:47 +0200

    Make box mutable

--- src/main/java/de/vorb/tesseract/util/Box.java
+++ src/main/java/de/vorb/tesseract/util/Box.java
@@ -3,32 +3,60 @@ package de.vorb.tesseract.util;
 import java.awt.Rectangle;
 
 public class Box {
-    private final int x;
-    private final int y;
-    private final int width;
-    private final int height;
+    private int x;
+    private int y;
+    private int width;
+    private int height;
 
     public Box(int x, int y, int width, int height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        setX(x);
+        setY(y);
+        setWidth(width);
+        setHeight(height);
     }
 
     public int getX() {
         return x;
     }
 
+    public void setX(int x) {
+        if (x < 0)
+            throw new IllegalArgumentException("negative coordinate x");
+
+        this.x = x;
+    }
+
     public int getY() {
         return y;
+    }
+
+    public void setY(int y) {
+        if (y < 0)
+            throw new IllegalArgumentException("negative coordinate y");
+
+        this.y = y;
     }
 
     public int getWidth() {
         return width;
     }
 
+    public void setWidth(int width) {
+        if (width < 0)
+            throw new IllegalArgumentException("negative width");
+
+        this.width = width;
+    }
+
     public int getHeight() {
         return height;
+    }
+
+    public void setHeight(int height) {
+        if (height < 0)
+            throw new IllegalArgumentException("negative height");
+
+        this.height = height;
     }
 
     public Rectangle toRectangle() {
