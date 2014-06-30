fe2d1eea68c3d1a7a19d976df261e1b62365ca83
Paul Vorbach <paul@vorba.ch>
2014-06-30 21:17:43 +0200

    Canvas and BoxFileCanvas (currently unused)

---
+++ src/main/java/de/vorb/tesseract/gui/view/BoxFileCanvas.java
@@ -0,0 +1,72 @@
+package de.vorb.tesseract.gui.view;
+
+import static de.vorb.tesseract.gui.model.Scale.scaled;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.ListModel;
+
+import com.google.common.base.Optional;
+
+import de.vorb.tesseract.util.Box;
+import de.vorb.tesseract.util.Symbol;
+
+public class BoxFileCanvas extends Canvas {
+    private static final long serialVersionUID = 1L;
+
+    private float scale;
+    private Optional<ListModel<Symbol>> symbols =
+            Optional.<ListModel<Symbol>> absent();
+    private int selectedIndex = -1;
+
+    public BoxFileCanvas() {
+    }
+
+    public void setScale(float scale) {
+        this.scale = scale;
+    }
+
+    public void setSymbols(Optional<ListModel<Symbol>> symbols,
+            int selectedIndex) {
+        this.symbols = symbols;
+        this.selectedIndex = -1;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (!this.symbols.isPresent())
+            return;
+
+        final ListModel<Symbol> symbols = this.symbols.get();
+        final int size = symbols.getSize();
+
+        for (int i = 0; i < size; i++) {
+            final Symbol symbol = symbols.getElementAt(i);
+            drawSymbolBox((Graphics2D) g, symbol, scale, i == selectedIndex);
+        }
+    }
+
+    private void drawSymbolBox(final Graphics2D g, final Symbol symbol,
+            final float scale, final boolean isSelected) {
+        final Box bbox = symbol.getBoundingBox();
+
+        // set selected colors
+        if (isSelected) {
+            g.setColor(Colors.SELECTION);
+            g.setStroke(Strokes.SELECTION);
+        }
+
+        // draw the box
+        g.drawRect(scaled(bbox.getX(), scale), scaled(bbox.getY(), scale),
+                scaled(bbox.getWidth(), scale), scaled(bbox.getHeight(), scale));
+
+        // unset selected colors
+        if (isSelected) {
+            g.setColor(Colors.NORMAL);
+            g.setStroke(Strokes.NORMAL);
+        }
+    }
+}
--- src/main/java/de/vorb/tesseract/gui/view/Canvas.java
+++ src/main/java/de/vorb/tesseract/gui/view/Canvas.java
@@ -6,30 +6,22 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
 
-import javax.swing.JPanel;
+import javax.swing.JComponent;
 
 import com.google.common.base.Optional;
 
-public class Canvas extends JPanel {
+public class Canvas extends JComponent {
     private static final long serialVersionUID = 1L;
 
     private static final Dimension DIM_EMPTY = new Dimension(0, 0);
 
     private Optional<Image> image = Optional.<Image> absent();
 
+    public Canvas() {
+    }
+
     public void setImage(Optional<Image> image) {
         this.image = image;
-
-        final Dimension size;
-        if (image.isPresent()) {
-            size = new Dimension(image.get().getWidth(null),
-                    image.get().getHeight(null));
-        } else {
-            size = DIM_EMPTY;
-        }
-
-        setSize(size);
-        setPreferredSize(size);
     }
 
     @Override
@@ -37,11 +29,22 @@ public class Canvas extends JPanel {
         final Rectangle rect = getVisibleRect();
 
         if (image.isPresent()) {
+            g.setClip(rect.x, rect.y, rect.width, rect.height);
             g.drawImage(image.get(), 0, 0, null);
         } else {
             g.setColor(Color.WHITE);
             g.fillRect((int) rect.getX(), (int) rect.getY(),
                     (int) rect.getWidth(), (int) rect.getHeight());
         }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        if (!image.isPresent()) {
+            return DIM_EMPTY;
+        }
+
+        return new Dimension(image.get().getWidth(null), image.get().getHeight(
+                null));
     }
 }
