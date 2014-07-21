fdc58b3cd0346787d021a9ac978460ca45fcffd4
Paul Vorbach <paul@vorba.ch>
2014-07-21 21:31:19 +0200

    Feature3D and 4D

--- src/main/java/de/vorb/tesseract/util/feat/Feature4D.java
+++ src/main/java/de/vorb/tesseract/util/feat/Feature4D.java
@@ -1,45 +1,43 @@
-package de.vorb.tesseract.traineddata;
+package de.vorb.tesseract.util.feat;
 
-public class IntProto {
+public class Feature4D {
     private final byte a;
     private final byte b; // unsigned byte
     private final byte c;
     private final byte angle; // unsigned byte
     private final int[] configs;
 
-    public IntProto(byte a, short b, byte c, short angle, int[] configs) {
-        this.a = a;
+    public Feature4D(int a, int b, int c, int angle, int[] configs) {
+        this.a = (byte) a;
         this.b = (byte) b;
-        this.c = c;
+        this.c = (byte) c;
         this.angle = (byte) angle;
         this.configs = configs;
     }
 
-    public byte getA() {
+    public int getA() {
         return a;
     }
 
-    public short getB() {
-        return (short) (b & 0xFF);
+    public int getB() {
+        return b & 0xFF;
     }
 
-    public byte getC() {
+    public int getC() {
         return c;
     }
 
-    public short getAngle() {
-        return (short) (angle & 0xFF);
+    public int getAngle() {
+        return angle & 0xFF;
     }
 
     public long getConfig(int i) {
         return configs[i] & 0xFFFF_FFFFL;
     }
-    
-    
 
     @Override
     public String toString() {
-        return String.format("IntProto(a=%d, b=%d, c=%d, angle=%d)", getA(),
+        return String.format("Feature4D(a=%d, b=%d, c=%d, angle=%d)", getA(),
                 getB(), getC(), getAngle());
     }
 }
