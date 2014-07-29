9db98e90f3f0e2b592d8a35c51a207bcce9dbb0f
Paul Vorbach <paul@vorba.ch>
2014-07-29 15:13:00 +0200

    Char and CharacterDimensions

---
+++ src/main/java/de/vorb/tesseract/traineddata/Char.java
@@ -0,0 +1,82 @@
+package de.vorb.tesseract.traineddata;
+
+import de.vorb.tesseract.tools.training.CharacterProperties;
+
+public class Char {
+    private final String text;
+    private final CharacterProperties props;
+    private final CharacterDimensions dims;
+    private final String script;
+    private final int otherCase;
+    private final int dir;
+    private final int mirror;
+    private final String normed;
+
+    public Char(String text, CharacterProperties properties,
+            CharacterDimensions dimensions, String script, int otherCase,
+            int direction,
+            int mirror, String normed) {
+        this.text = text;
+        this.props = properties;
+        this.dims = dimensions;
+        this.script = script;
+        this.otherCase = otherCase;
+        this.dir = direction;
+        this.mirror = mirror;
+        this.normed = normed;
+    }
+
+    public Char(String text, CharacterProperties properties, String script,
+            int otherCase) {
+        this(text, properties, CharacterDimensions.DEFAULT, script, otherCase,
+                0, 0, "");
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public CharacterProperties getProperties() {
+        return props;
+    }
+
+    public CharacterDimensions getDimensions() {
+        return dims;
+    }
+
+    public String getScript() {
+        return script;
+    }
+
+    public int getOtherCase() {
+        return otherCase;
+    }
+
+    public int getDirection() {
+        return dir;
+    }
+
+    public int getMirror() {
+        return mirror;
+    }
+
+    public String getNormed() {
+        return normed;
+    }
+
+    @Override
+    public String toString() {
+        if (text.equals(" ")) {
+            return "NULL 0 NULL 0";
+        } else {
+            return String.format(
+                    "%s %d %d,%d,%d,%d,%d,%d,%d,%d,%d,%d %s %d %d %d %s",
+                    text, props.toByteCode(), dims.getMinBottom(),
+                    dims.getMaxBottom(), dims.getMinTop(), dims.getMaxTop(),
+                    dims.getMinWidth(), dims.getMaxWidth(),
+                    dims.getMinBearing(), dims.getMaxBearing(),
+                    dims.getMinAdvance(), dims.getMaxAdvance(), script,
+                    otherCase, dir, mirror, normed);
+        }
+    }
+}
--- src/main/java/de/vorb/tesseract/traineddata/Unicharset.java
+++ src/main/java/de/vorb/tesseract/traineddata/Unicharset.java
@@ -1,14 +1,80 @@
 package de.vorb.tesseract.traineddata;
 
-public class Unicharset {
-    private final int size;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public Unicharset(String str) {
-        final String[] lines = str.split("\n");
-        size = Integer.parseInt(lines[0]);
+import de.vorb.tesseract.tools.training.CharacterProperties;
+
+public class Unicharset {
+    private static final Pattern LONG_LINE =
+            Pattern.compile("^(\\S+) (\\d+) (\\d+),(\\d+),(\\d+),(\\d+),(\\d+),"
+                    + "(\\d+),(\\d+),(\\d+),(\\d+),(\\d+) (\\S+) (\\d+) (\\d+) "
+                    + "(\\d+) (.+)$");
+    private static final Pattern FIRST_LINE =
+            Pattern.compile("^NULL 0 NULL 0$");
+    private static final Pattern DELIM = Pattern.compile("\\s+|,");
+
+    private List<Char> characters;
+
+    public Unicharset(List<Char> charset) {
+        this.characters = charset;
     }
 
-    public int getSize() {
-        return size;
+    public List<Char> getCharacters() {
+        return Collections.unmodifiableList(characters);
+    }
+
+    public static Unicharset readFrom(BufferedReader in) throws IOException {
+        final int size = Integer.parseInt(in.readLine());
+        final List<Char> charset = new ArrayList<>(size);
+
+        String line;
+        Scanner scanner;
+        while ((line = in.readLine()) != null) {
+            if (LONG_LINE.matcher(line).matches()) {
+                scanner = new Scanner(line);
+                scanner.useDelimiter(DELIM);
+
+                final String text = scanner.next();
+                final CharacterProperties props =
+                        CharacterProperties.forByteCode(scanner.nextByte());
+                final CharacterDimensions dims = new CharacterDimensions(
+                        scanner.nextInt() /* min bottom */,
+                        scanner.nextInt() /* max bottom */,
+                        scanner.nextInt() /* min top */,
+                        scanner.nextInt() /* max top */,
+                        scanner.nextInt() /* min width */,
+                        scanner.nextInt() /* max width */,
+                        scanner.nextInt() /* min bearing */,
+                        scanner.nextInt() /* max bearing */,
+                        scanner.nextInt() /* min advance */,
+                        scanner.nextInt() /* max advance */);
+                final String script = scanner.next();
+                final int otherCase = scanner.nextInt();
+                final int direction = scanner.nextInt();
+                final int mirror = scanner.nextInt();
+                final String normed = scanner.nextLine().trim();
+                charset.add(new Char(text, props, dims, script, otherCase,
+                        direction, mirror, normed));
+
+                scanner.close();
+            } else if (FIRST_LINE.matcher(line).matches()) {
+                charset.add(new Char(" ",
+                        CharacterProperties.forByteCode((byte) 0), " ", 0));
+            }
+        }
+
+        for (Char c : charset) {
+            System.out.println(c);
+        }
+
+        return new Unicharset(charset);
     }
 }
