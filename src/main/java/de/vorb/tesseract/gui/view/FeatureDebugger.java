bf03edc9459e15e21c3e5c2e36c23acfaecea56c
Paul Vorbach <paul@vorba.ch>
2014-07-31 12:14:30 +0200

    Improve feature debugger

--- src/main/java/de/vorb/tesseract/gui/view/FeatureDebugger.java
+++ src/main/java/de/vorb/tesseract/gui/view/FeatureDebugger.java
@@ -2,31 +2,31 @@ package de.vorb.tesseract.gui.view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.Window;
-import java.util.List;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import de.vorb.tesseract.util.feat.Feature3D;
-import de.vorb.tesseract.util.feat.Feature4D;
-
-import javax.swing.JLabel;
-
-import java.awt.Dimension;
 import java.awt.geom.Line2D;
 import java.awt.geom.Line2D.Double;
 import java.awt.image.BufferedImage;
+import java.util.List;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.google.common.base.Optional;
+
+import de.vorb.tesseract.tools.training.IntClass;
+import de.vorb.tesseract.tools.training.IntTemplates;
+import de.vorb.tesseract.util.feat.Feature3D;
+import de.vorb.tesseract.util.feat.Feature4D;
 
 public class FeatureDebugger extends JDialog {
     private static final long serialVersionUID = 1L;
@@ -40,6 +40,9 @@ public class FeatureDebugger extends JDialog {
     private final BufferedImage canvas = new BufferedImage(WIDTH, HEIGHT,
             BufferedImage.TYPE_INT_RGB);
     private final JLabel lblCanvas;
+
+    private List<Feature3D> features;
+    private Optional<IntTemplates> prototypes = Optional.absent();
 
     /**
      * Create the dialog.
@@ -85,16 +88,40 @@ public class FeatureDebugger extends JDialog {
     }
 
     public void setFeatures(List<Feature3D> features) {
+        this.features = features;
+        redraw();
+    }
+
+    public void setPrototypes(Optional<IntTemplates> prototypes) {
+        this.prototypes = prototypes;
+    }
+
+    private void redraw() {
         final Graphics2D g2d = canvas.createGraphics();
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setColor(Color.BLACK);
         g2d.setBackground(Color.WHITE);
         g2d.clearRect(0, 0, WIDTH, HEIGHT);
-        final Double line = new Line2D.Double(0, 0, 0, 0);
+
+        final Double line = new Line2D.Double(0d, 0d, 0d, 0d);
+        if (prototypes.isPresent()) {
+            final List<IntClass> classes = prototypes.get().getClasses();
+            for (final Feature4D feat : classes.get(0).getProtoSets().get(0).getProtos()) {
+                // transform the angle back to radians
+                final double angle = feat.getAngle() / 256d * PI2;
+                final double dx = Math.cos(angle) * feat.getC();
+                final double dy = Math.sin(angle) * feat.getC();
+                final double x1 = feat.getA();
+                final double x2 = feat.getA() + dx;
+                final double y1 = feat.getB();
+                final double y2 = feat.getB() + dy;
+                line.setLine(x1, y1, x2, y2);
+                g2d.draw(line);
+            }
+        }
+
         for (final Feature3D feat : features) {
-            if ((feat.getX() == 0 || feat.getY() == 0) && feat.getTheta() == 0)
-                continue;
             // transform the angle back to radians
             final double theta = feat.getTheta() / 256d * PI2;
             final double dx = Math.cos(theta) * FEATURE_RADIUS;
