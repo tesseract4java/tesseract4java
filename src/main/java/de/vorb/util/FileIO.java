fdae9f41ccc379a22bd6438f2195f538ae8a3d87
Paul Vorbach <paul@vorba.ch>
2014-08-01 11:08:21 +0200

    Rename parameter

--- src/main/java/de/vorb/util/FileIO.java
+++ src/main/java/de/vorb/util/FileIO.java
@@ -1,12 +1,10 @@
 package de.vorb.util;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
@@ -55,16 +53,14 @@ public class FileIO {
             @Override
             public FileVisitResult visitFile(Path file,
                     BasicFileAttributes attrs)
-                    throws IOException
-            {
+                    throws IOException {
                 Files.delete(file);
                 return FileVisitResult.CONTINUE;
             }
 
             @Override
-            public FileVisitResult visitFileFailed(Path file, IOException exc)
-                    throws IOException
-            {
+            public FileVisitResult visitFileFailed(Path file, IOException e)
+                    throws IOException {
                 // try to delete the file anyway, even if its attributes
                 // could not be read, since delete-only access is
                 // theoretically possible
@@ -73,18 +69,14 @@ public class FileIO {
             }
 
             @Override
-            public FileVisitResult postVisitDirectory(Path dir, IOException exc)
-                    throws IOException
-            {
-                if (exc == null)
-                {
+            public FileVisitResult postVisitDirectory(Path dir, IOException e)
+                    throws IOException {
+                if (e == null) {
                     Files.delete(dir);
                     return FileVisitResult.CONTINUE;
-                }
-                else
-                {
+                } else {
                     // directory iteration failed; propagate exception
-                    throw exc;
+                    throw e;
                 }
             }
         });
