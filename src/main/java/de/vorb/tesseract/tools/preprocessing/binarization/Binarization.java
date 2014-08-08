10e059551983e548e11ad9b30d2f131062354b62
Paul Vorbach <paul@vorba.ch>
2014-08-08 11:30:07 +0200

    Fix Binarization

---
+++ src/main/java/de/vorb/tesseract/tools/preprocessing/binarization/Binarization.java
@@ -0,0 +1,19 @@
+package de.vorb.tesseract.tools.preprocessing.binarization;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Binarization algorithm.
+ * 
+ * @author Paul Vorbach
+ */
+public interface Binarization {
+    /**
+     * Binarize an image.
+     * 
+     * @param image
+     *            input image
+     * @return binary image
+     */
+    BufferedImage apply(BufferedImage image);
+}
