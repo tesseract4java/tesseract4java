c503cee02917d8408e7ccfcd28b2165c43903cf0
Paul Vorbach <paul@vorba.ch>
2014-09-07 14:35:22 +0200

    Fix FileNames

--- src/main/java/de/vorb/util/FileNames.java
+++ src/main/java/de/vorb/util/FileNames.java
@@ -4,9 +4,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileNames {
-    public static Path replaceExtension(Path fileName, String newExtension) {
-        final String fname = fileName.toString();
+    public static Path replaceExtension(Path file, String newExtension) {
+        final String fname = file.getFileName().toString();
         final int lastDot = fname.lastIndexOf('.');
-        return Paths.get(fname.substring(0, lastDot + 1) + newExtension);
+        if (file.getParent() == null) {
+            return Paths.get(fname.substring(0, lastDot + 1) + newExtension);
+        } else {
+            return file.getParent().resolve(
+                    fname.substring(0, lastDot + 1) + newExtension);
+        }
     }
 }
