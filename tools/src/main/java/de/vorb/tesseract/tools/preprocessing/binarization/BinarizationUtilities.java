8d1a383fdec203b02134e34e4ee31fce4950f884
Paul Vorbach <paul@vorba.ch>
2019-07-09 01:16:48 +0200

    Add support for TYPE_3BYTE_BGR images
    Fixes #42

--- tools/src/main/java/de/vorb/tesseract/tools/preprocessing/binarization/BinarizationUtilities.java
+++ tools/src/main/java/de/vorb/tesseract/tools/preprocessing/binarization/BinarizationUtilities.java
@@ -6,31 +6,31 @@ import java.awt.image.ColorConvertOp;
 
 public final class BinarizationUtilities {
 
-    private BinarizationUtilities() {}
+    private BinarizationUtilities() {
+    }
 
     private static final ColorConvertOp RGB_TO_GRAYSCALE = new ColorConvertOp(
             ColorSpace.getInstance(ColorSpace.CS_sRGB),
             ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
 
-    public static BufferedImage imageToGrayscale(BufferedImage image) {
+    static BufferedImage imageToGrayscale(BufferedImage image) {
         final BufferedImage grayscale;
 
-        // return the buffered image as-is, if it is binary already
-        if (image.getType() == BufferedImage.TYPE_BYTE_BINARY) {
-            return image;
-        } else if (image.getType() == BufferedImage.TYPE_BYTE_GRAY) {
-            grayscale = image;
-        } else if (image.getType() == BufferedImage.TYPE_INT_RGB
-                || image.getType() == BufferedImage.TYPE_BYTE_INDEXED) {
-            grayscale = new BufferedImage(image.getWidth(), image.getHeight(),
-                    BufferedImage.TYPE_BYTE_GRAY);
-
-            // convert rgb image to grayscale
-            RGB_TO_GRAYSCALE.filter(image, grayscale);
-        } else {
-            throw new IllegalArgumentException(String.format(
-                    "illegal color space: %s",
-                    image.getColorModel().getColorSpace().getType()));
+        switch (image.getType()) {
+            case BufferedImage.TYPE_BYTE_BINARY:
+                return image;
+            case BufferedImage.TYPE_BYTE_GRAY:
+                grayscale = image;
+                break;
+            case BufferedImage.TYPE_INT_RGB:
+            case BufferedImage.TYPE_BYTE_INDEXED:
+            case BufferedImage.TYPE_3BYTE_BGR:
+                grayscale = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+                RGB_TO_GRAYSCALE.filter(image, grayscale);
+                break;
+            default:
+                throw new IllegalArgumentException(
+                        "illegal color space: " + image.getColorModel().getColorSpace().getType());
         }
 
         return grayscale;
